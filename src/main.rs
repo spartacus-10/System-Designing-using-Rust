3a416e862596ee6928f9e9e71f0b05574ccdea39
spartacus-10 <ak2003eftychia@gmail.com>
2025-09-09 23:41:00 +0530

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -14,9 +15,33 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024]; //to hold the data that is read
 
     stream.read(&mut buffer).unwrap();
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        //to read the contents
+        let contents = fs::read_to_string("index.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length:{}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        //to read the contents
+        let contents = fs::read_to_string("error.html").unwrap();
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 
     //println!("Request:{}", String::from_utf8_lossy(&buffer[..]));
 }
